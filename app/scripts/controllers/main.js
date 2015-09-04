1f2d5a679692f65c402f3cf7de3efed59d088379
José Laya <jmlaya@gmail.com>
2015-09-04 00:34:34 -0430

    Rebuild for fix title page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,22 +17,16 @@ angular.module('binowPlayingApp')
     });
 
 //inject the twitterService into the controller
-angular.module('binowPlayingApp').controller('TwitterController', function ($scope, $q, twitterService, $sce, $timeout) {
+angular.module('binowPlayingApp').controller('TwitterController', function ($scope, $q, twitterService, $sce, $timeout, Utils) {
 
     $scope.tweets; //array of tweets
 
+
+    //Necesary to bind "unsecure" url of youtube video using angular
     $scope.trustSrc = function (src) {
         var code = src.split('/');
         return $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + code[code.length - 1]);
-    }
-
-    $scope.getTweet = function (id) {
-        var defer = $q.defer
-        twitterService.getTweet(id).then(function (tweet) {
-
-        })
-        return defer.promise;
-    }
+    };
 
     twitterService.initialize();
 
@@ -43,7 +37,7 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
             $scope.tweets = data.statuses;
 
         });
-    }
+    };
 
     //when the user clicks the connect twitter button, the popup authorization window opens
     $scope.connectButton = function () {
@@ -67,6 +61,7 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
         });
     };
 
+    //Post tweet functionality to try to send coordinates to it
     $scope.postTweet = function () {
         twitterService.postTweet($scope.comment + ' ' + $scope.video).then(function () {
             $scope.video = "";
@@ -75,46 +70,9 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
         })
     };
 
+    //Simple function to genetate dates ago (1 day ago)
     $scope.calculateSince = function (datetime) {
-        var tTime = new Date(datetime);
-        var cTime = new Date();
-        var sinceMin = Math.round((cTime - tTime) / 60000);
-        if (sinceMin == 0) {
-            var sinceSec = Math.round((cTime - tTime) / 1000);
-            if (sinceSec < 10)
-                var since = 'less than 10 seconds ago';
-            else if (sinceSec < 20)
-                var since = 'less than 20 seconds ago';
-            else
-                var since = 'half a minute ago';
-        }
-        else if (sinceMin == 1) {
-            var sinceSec = Math.round((cTime - tTime) / 1000);
-            if (sinceSec == 30)
-                var since = 'half a minute ago';
-            else if (sinceSec < 60)
-                var since = 'less than a minute ago';
-            else
-                var since = '1 minute ago';
-        }
-        else if (sinceMin < 45)
-            var since = sinceMin + ' minutes ago';
-        else if (sinceMin > 44 && sinceMin < 60)
-            var since = 'about 1 hour ago';
-        else if (sinceMin < 1440) {
-            var sinceHr = Math.round(sinceMin / 60);
-            if (sinceHr == 1)
-                var since = 'about 1 hour ago';
-            else
-                var since = 'about ' + sinceHr + ' hours ago';
-        }
-        else if (sinceMin > 1439 && sinceMin < 2880)
-            var since = '1 day ago';
-        else {
-            var sinceDay = Math.round(sinceMin / 1440);
-            var since = sinceDay + ' days ago';
-        }
-        return since;
+        return Utils.calculateSince(datetime);
     };
 
     //if the user is a returning user, hide the sign in button and display the tweets
@@ -126,5 +84,9 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
     else {
         $('#getTimelineButton, #signOut, .withlogin').hide();
     }
+
+    $timeout(function () {
+        $scope.refreshTimeline();
+    }, 60000);
 
 });
