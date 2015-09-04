7351670fef24c223af6130aa45b6de72354dc1a2
José Laya <jmlaya@gmail.com>
2015-09-03 22:30:34 -0430

    Retweet, Reply & Favorite

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,9 +26,9 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
         return $sce.trustAsResourceUrl('http://www.youtube.com/embed/' + code[code.length - 1]);
     }
 
-    $scope.getTweet = function(id){
+    $scope.getTweet = function (id) {
         var defer = $q.defer
-        twitterService.getTweet(id).then(function(tweet){
+        twitterService.getTweet(id).then(function (tweet) {
 
         })
         return defer.promise;
@@ -76,6 +76,48 @@ angular.module('binowPlayingApp').controller('TwitterController', function ($sco
 
     $scope.reTweet = function (id) {
         twitterService.reTweet(id);
+    };
+
+    $scope.calculateSince = function (datetime) {
+        var tTime = new Date(datetime);
+        var cTime = new Date();
+        var sinceMin = Math.round((cTime - tTime) / 60000);
+        if (sinceMin == 0) {
+            var sinceSec = Math.round((cTime - tTime) / 1000);
+            if (sinceSec < 10)
+                var since = 'less than 10 seconds ago';
+            else if (sinceSec < 20)
+                var since = 'less than 20 seconds ago';
+            else
+                var since = 'half a minute ago';
+        }
+        else if (sinceMin == 1) {
+            var sinceSec = Math.round((cTime - tTime) / 1000);
+            if (sinceSec == 30)
+                var since = 'half a minute ago';
+            else if (sinceSec < 60)
+                var since = 'less than a minute ago';
+            else
+                var since = '1 minute ago';
+        }
+        else if (sinceMin < 45)
+            var since = sinceMin + ' minutes ago';
+        else if (sinceMin > 44 && sinceMin < 60)
+            var since = 'about 1 hour ago';
+        else if (sinceMin < 1440) {
+            var sinceHr = Math.round(sinceMin / 60);
+            if (sinceHr == 1)
+                var since = 'about 1 hour ago';
+            else
+                var since = 'about ' + sinceHr + ' hours ago';
+        }
+        else if (sinceMin > 1439 && sinceMin < 2880)
+            var since = '1 day ago';
+        else {
+            var sinceDay = Math.round(sinceMin / 1440);
+            var since = sinceDay + ' days ago';
+        }
+        return since;
     };
 
     //if the user is a returning user, hide the sign in button and display the tweets
